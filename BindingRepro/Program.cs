1ef17db5d1711ddc9bf41e1c02987d2c64b103c2
Nick Craver <craver@stackoverflow.com>
2018-09-27 10:51:18 -0400

    Initial commit

--- BindingRepro/Program.cs
+++ BindingRepro/Program.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RefPipelines;
+using System;
 
 namespace BindingRepro
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Console.WriteLine("Going boom...");
+            Boom.CantTouchThis();
+            Console.WriteLine("Shouldn't get here...");
         }
     }
 }
---
+++ RefPipelines/Boom.cs
@@ -0,0 +1,10 @@
+﻿using System.Buffers;
+
+namespace RefPipelines
+{
+    public static class Boom
+    {
+        //public static ReadOnlySequence<int> CantTouchThis() => new ReadOnlySequence<int>();
+        public static void CantTouchThis() => System.IO.Pipelines.PipeOptions.Default.Pool.Rent(10000);
+    }
+}
